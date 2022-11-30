a9c210ec09585f7b6f8a1d39c3588eaa70cf3913
Any97Cris <criscianysilva1997@gmail.com>
2022-11-30 07:52:26 -0400

    logo e estilo inserido

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img class="text-center" src="/img/logo.png" alt="logo_mecanico">
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
@@ -28,16 +28,18 @@
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Lembrar') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        {{ __('Esqueceu a senha?') }}
                     </a>
                 @endif
+
+                <a href="/register" class="ml-4 underline text-sm">Cadastrar</a>
 
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
