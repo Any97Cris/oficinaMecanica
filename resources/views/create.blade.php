84fa3dd594b97aa239e8aa5260a2c727987040ce
Any97Cris <criscianysilva1997@gmail.com>
2022-11-30 17:27:15 -0400

    campos tela cadastrar

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -14,6 +14,52 @@
     <link rel="icon" href="/img/favicon.ico" type="image/x-icon">
 </head>
 <body>
+
+    <div class="container bg-light fundoDivPrincipal mt-5 p-4">
+        <form action="/create/cliente" method="POST">
+        <h3>Cadastro de Cliente</h3>
+        <div class="form-group mt-5 mb-3">
+            <label class="form-label">Nome</label>
+            <input type="text" name="name" id="name" class="form-control" required placeholder="Nome do Cliente">
+        </div style="display:flex;">
+        
+        <div class="form-group mt-2 mb-3">
+            <label class="form-label">Contato</label>
+            <input type="text" class="form-control" name="contact" id="contact" required placeholder="Número para contato">
+        </div>
+
+        <select class="form-select mb-5" aria-label="Default select example" required>
+            <option selected>Qual o tipo de Contato</option>
+            <option value="1">Empresa</option>
+            <option value="2">Residencial</option>
+            <option value="3">Pessoal</option>
+          </select>
+        
+        <label>Informçãoes do Carro</label>
+        <div class="form-group">
+            <label class="form-label">Modelo</label>
+            <input type="text" class="form-control" name="modelo" id="modelo" required placeholder="Digite o modelo do Carro...."/>
+        </div>
+        <div class="form-group">
+            <label class="form-label">Placa</label>
+            <input type="text" class="form-control" name="placa" id="placa" required placeholder="Digite o placa do Carro...."/>
+        </div>
+        <div class="form-group mb-5">
+            <label class="form-label">Marca</label>
+            <input type="text" class="form-control" name="marca" id="marca" required placeholder="Digite o marca do Carro...."/>
+        </div>
+
+        <label>Serviço - Pintura do Veículo</label>
+        <div class="form-group mb-5">
+            <label class="form-label">Cor</label>
+            <input type="text" class="form-control" name="cor" id="cor" required placeholder="Digite a cor para pintura..."/>
+        </div>
+        <div class="text-center">
+            <a href="/users" class="btn btn-warning">Página Inicial</a>
+            <input type="submit" class="btn btn-success">
+        </div>
+    </form>
+    </div>
     
 
     <!-- JavaScript Bundle with Popper -->
