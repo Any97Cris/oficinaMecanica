9eda4df0860812a0d908a6d63d7c777c5ea73557
Any97Cris <criscianysilva1997@gmail.com>
2022-11-30 07:52:07 -0400

    logo foi inserida

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img class="text-center" src="/img/logo.png" alt="logo_mecanico" width="60%" style="margin-left: 70px;">
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
@@ -48,11 +48,11 @@
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    {{ __('Já tem cadastro?') }}
                 </a>
 
                 <x-jet-button class="ml-4">
-                    {{ __('Register') }}
+                    {{ __('Registrar') }}
                 </x-jet-button>
             </div>
         </form>
